df5378f0457438a25afda804cbf7b7360be20f3c
10ErickHerrera <10erickherrera@gmail.com>
2024-10-17 00:42:29 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,60 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".boton").addEventListener("click", generarCarta);
+  generarCarta();
 };
+
+function generarCarta() {
+  const palos = ["♠", "♥", "♦", "♣"];
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function numeroAleatorio(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  const paloIndex = numeroAleatorio(4);
+  const numeroIndex = numeroAleatorio(13);
+
+  const simboloArriba = document.querySelector(".simbolo-arriba");
+  const simboloAbajo = document.querySelector(".simbolo-abajo");
+  const numeroCentro = document.querySelector(".numero");
+
+  const palo = palos[paloIndex];
+  const numero = numeros[numeroIndex];
+
+  simboloArriba.textContent = palo;
+  simboloAbajo.textContent = palo;
+  numeroCentro.textContent = numero;
+
+  if (palo === "♥" || palo === "♦") {
+    simboloArriba.classList.add("rojo");
+    simboloAbajo.classList.add("rojo");
+    numeroCentro.classList.add("rojo");
+    simboloArriba.classList.remove("negro");
+    simboloAbajo.classList.remove("negro");
+    numeroCentro.classList.remove("negro");
+  } else {
+    simboloArriba.classList.add("negro");
+    simboloAbajo.classList.add("negro");
+    numeroCentro.classList.add("negro");
+    simboloArriba.classList.remove("rojo");
+    simboloAbajo.classList.remove("rojo");
+    numeroCentro.classList.remove("rojo");
+  }
+}
